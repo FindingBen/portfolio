ba89ab1c321d9b37cdbb0492fee4733bfe5bf7f5
Benjamin <beniagic@gmail.com>
2024-01-08 09:23:55 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/portfolio/",
+  basePath: "/portfolio-app/",
 };
 
 module.exports = nextConfig;
